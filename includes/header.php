35bc79232a7d9b322d6d0bed1ef12f771862d66d
Glenda <glendaeea1@gmail.com>
2023-12-18 15:59:37 -0300

    first commit

---
+++ includes/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+        
+        <meta charset="UTF-8">
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <link rel="stylesheet" href="../assets/css/patterns.css">
+    
+    </head>
+
+    <body>
+
+        <!-- DIV with the header content -->
+        <div class="center">
+            
+            <!-- MENU ICON -->
+            <button type="button" class="buttonmenu"> </button>
+            <img src="../assets/img/menuIcon.png" alt="Menu icon image. Three green little lines." class="menuicon">
+            
+            <!-- USER PROFILE ICON -->
+            <button type="button" class="buttonusericon"> </button>
+            <img src="../assets/img/user.png" alt="" class="usericon">
+            
+            <!-- FOR YOU ICON -->
+            <button type="button" class="buttontimeline"> </button>
+            <img src="../assets/img/timelineicon.png" alt="" class="timelineicon">
+            
+            <!-- FRIENDS TIMELINE ICON -->
+            <button type="button" class="buttonfriends"> </button>
+            <img src="../assets/img/friendstimeline.png" alt="" class="friendsicon">
+       
+        </div>
+
+    </body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+    $headerPath = __DIR__ . '/includes/header.php';
+
+    if (file_exists($headerPath))
+    {
+        include($headerPath);
+    }
+    else
+    {
+        http_response_code(404);
+        
+        echo "Couldn't find header file in " . $headerPath;
+    }
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+        
+        <meta charset="UTF-8">
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <link rel="stylesheet" href="../assets/css/patterns.css">
+
+        <title>SoundnShare</title>
+    
+    </head>
+
+    <body>
+
+    </body>
+
+</html>
